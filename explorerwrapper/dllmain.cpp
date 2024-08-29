7a846ac1ba4ccbca0c0541e4487f9540f5572939
wiktorwiktor12 <goodbloxhacks@gmail.com>
2024-08-29 23:57:42 +0100

    custom remakes of nsctree functions to stop hooks and patches from interfering
    thank you aerexplorer and mfs who patch explorerframe.dll

--- explorerwrapper/dllmain.cpp
+++ explorerwrapper/dllmain.cpp
@@ -24,6 +24,7 @@
 #include "MinHook.h"
 #include "taskscheduler.h"
 #include "registry.h"
+#include "nsctree.h"
 
 #define _WIN_BLUE 1 //Win8.1-specific changes
 #define _WIN_TH1 0 //Win10TH1-specific changes - currently unused
@@ -737,67 +738,6 @@ BOOL WINAPI CalculatePopupWindowPositionNEW(
 	return res;
 }
 
-bool dirExists(const WCHAR* dirName_in) //ripped from here https://stackoverflow.com/questions/8233842/how-to-check-if-directory-exist-using-c-and-winapi 
-{
-	DWORD ftyp = GetFileAttributesW(dirName_in);
-	if (ftyp == INVALID_FILE_ATTRIBUTES)
-		return false;
-
-	if (ftyp & FILE_ATTRIBUTE_DIRECTORY)
-		return true;
-
-	return false;
-}
-
-MIDL_INTERFACE("00000000-0000-0000-0000-000000000000")
-INameSpaceTreeControlValuesPrivate : IUnknown
-{
-public:
-	virtual void stub() = 0;
-	virtual void SetIndentValue(int indent) = 0;
-};
-
-HRESULT __fastcall CNSCHost_FillNSC(uintptr_t nscHost) //todo: reimplement the filter from 7 shell32, CLSID_PersonalStartMenu GUID_2659b475_eeb8_48b7_8f07_b378810f48cf
-{
-	HRESULT result = S_OK; 
-	if (*(DWORD*)(nscHost + 0xCC)) return result;
-
-	WCHAR fallbackPath[MAX_PATH];
-	ExpandEnvironmentStringsW(L"%PROGRAMDATA%\\Microsoft\\Windows\\Start Menu\\Programs", fallbackPath, MAX_PATH);
-
-	WCHAR* sibAllProgs = L"shell:::{865e5e76-ad83-4dca-a109-50dc2113ce9b}";
-
-	IShellItem* ppv;
-	if ((dirExists(sibAllProgs) && SHCreateItemFromParsingName(
-		sibAllProgs,
-		0LL,
-		IID_IShellItem,
-		(void**)&ppv) == S_OK) || SHCreateItemFromParsingName(
-			fallbackPath,
-			0LL,
-			IID_IShellItem,
-			(void**)&ppv) == S_OK)
-	{
-		INameSpaceTreeControl2* control = *(INameSpaceTreeControl2**)(nscHost + 0x70);
-
-		//ideally we would want to queryinterface to get these, but im too lazy to get the guids for these, and i doubt these offsets would change
-		IVisualProperties* visualProps = (IVisualProperties*)(__int64(control) + 0x20);
-		INameSpaceTreeControlValuesPrivate* privatec = (INameSpaceTreeControlValuesPrivate*)(__int64(control) + 0x50);
-		privatec->SetIndentValue(13);
-		int itemheight = 0;
-		visualProps->GetItemHeight(&itemheight);
-		itemheight += 1;
-		visualProps->SetItemHeight(itemheight);
-
-		result = control->AppendRoot(ppv,96,3,0);
-		if (SUCCEEDED(result))
-			*(DWORD*)(nscHost + 0xCC) = 1;
-
-		ppv->Release();
-	}
-	return result;
-}
-
 void HookShell32();
 void HookAPIs()
 {
@@ -825,7 +765,7 @@ void HookAPIs()
 	MH_CreateHook(static_cast<LPVOID>(fOpenThemeDataForDpi), OpenThemeDataForDpi_Hook, reinterpret_cast<LPVOID*>(&fOpenThemeDataForDpi));
 	MH_CreateHook(static_cast<LPVOID>(fOpenThemeDataEx), OpenThemeDataEx_Hook, reinterpret_cast<LPVOID*>(&fOpenThemeDataEx));
 	if (fillnsc && g_osVersion.BuildNumber() >= 14393)
-		MH_CreateHook(static_cast<LPVOID>(fillnsc), CNSCHost_FillNSC, reinterpret_cast<LPVOID*>(&fillnsc));
+		MH_CreateHook(static_cast<LPVOID>(fillnsc), CNSCHost_FillNSC, reinterpret_cast<LPVOID*>(&fillnsc)); //this hook is in nsctree.h now
 	HookTrayThread();
 	MH_EnableHook(MH_ALL_HOOKS);
 
@@ -1107,10 +1047,10 @@ extern "C" HRESULT WINAPI Explorer_CoCreateInstance(
 	{
 		dbgprintf(L"CUSTOMDESTLIST!!!!\n");
 		result = CoCreateInstance(rclsid, pUnkOuter, dwClsContext, IID_CustomDestList10, ppv);
-		if (result != S_OK)
+		if (result != S_OK || !*ppv)
 		{
 			result = CoCreateInstance(rclsid, pUnkOuter, dwClsContext, IID_CustomDestList1507, ppv);
-			*ppv = new CCustomDestWrapper((IInternalCustomDestList10*)*ppv);
+			*ppv = new CCustomDestWrapper((IInternalCustomDestList1507*)*ppv);
 		}
 		else
 			*ppv = new CCustomDestWrapper((IInternalCustomDestList10*)*ppv);
---
+++ explorerwrapper/nsctree.h
@@ -0,0 +1,160 @@
+#pragma once
+#include <windows.h>
+#include <winuser.h>
+
+static void __fastcall SHComputeDPI(HWND a1, int* a2, int* a3)
+{
+	DPI_AWARENESS_CONTEXT v7; // rax
+	HDC DC; // rax
+	HDC v9; // rdi
+	int DeviceCaps; // ebx
+	int v11; // esi
+
+	if (a1 && (v7 = GetWindowDpiAwarenessContext(a1), AreDpiAwarenessContextsEqual(v7, (DPI_AWARENESS_CONTEXT)-4LL)))
+	{
+		DeviceCaps = GetDpiForWindow(a1);
+		v11 = DeviceCaps;
+	}
+	else
+	{
+		DC = GetDC(0LL);
+		v9 = DC;
+		if (DC)
+		{
+			DeviceCaps = GetDeviceCaps(DC, 88);
+			v11 = GetDeviceCaps(v9, 90);
+			ReleaseDC(0LL, v9);
+		}
+		else
+		{
+			DeviceCaps = 96;
+			v11 = 96;
+		}
+	}
+	if (a2)
+		*a2 = DeviceCaps;
+	if (a3)
+		*a3 = v11;
+}
+
+//custom versions of the functions because mfs use patched dlls and aerexplorer fucks this shit up
+static void __fastcall CNscTree_ScaleAndSetIndent(__int64 a1)
+{
+	int v1; // ebx
+	int v3; // eax
+	int nNumerator; // [rsp+30h] [rbp+8h] BYREF
+	int v6; // [rsp+38h] [rbp+10h] BYREF
+
+	v1 = *(DWORD*)(a1 + 0x1D0);
+	SHComputeDPI(*(HWND*)(a1 + 0x188), &v6, &nNumerator);
+	v3 = MulDiv(v1, nNumerator, 96);
+	SendMessageW(*(HWND*)(a1 + 0x178), 0x1107u, v3, 0LL);
+}
+
+static void __fastcall CNscTree_SetIndentValue(__int64 a1, int a2)
+{
+	*(DWORD*)(a1 + 0xA0) = a2;
+	CNscTree_ScaleAndSetIndent(a1 - 304);
+}
+
+static void __fastcall CNscTree_ScaleAndSetRowHeight(__int64 a1)
+{
+	int v1; // ebp
+	HWND v2; // rdi
+	DPI_AWARENESS_CONTEXT v5; // rax
+	HDC DC; // rax
+	HDC v7; // rbx
+	int DeviceCaps; // edi
+	int v9; // eax
+
+	v1 = *(DWORD*)(a1 + 0x1C8);
+	v2 = *(HWND*)(a1 + 0x188);
+	if (v2 && (v5 = GetWindowDpiAwarenessContext(v2), AreDpiAwarenessContextsEqual(v5, (DPI_AWARENESS_CONTEXT)-4LL)))
+	{
+		DeviceCaps = GetDpiForWindow(v2);
+	}
+	else
+	{
+		DC = GetDC(0LL);
+		v7 = DC;
+		if (DC)
+		{
+			GetDeviceCaps(DC, 88);
+			DeviceCaps = GetDeviceCaps(v7, 90);
+			ReleaseDC(0LL, v7);
+		}
+		else
+		{
+			DeviceCaps = 96;
+		}
+	}
+	v9 = MulDiv(v1, DeviceCaps, 96);
+	SendMessageW(*(HWND*)(a1 + 376), 0x111Bu, v9, 0LL);
+}
+
+static __int64 __fastcall CNscTree_SetItemHeight(__int64 a1, int a2)
+{
+	*(DWORD*)(a1 + 200) = a2;
+	CNscTree_ScaleAndSetRowHeight(a1 - 256);
+	return 0LL;
+}
+
+static bool dirExists(const WCHAR* dirName_in) //ripped from here https://stackoverflow.com/questions/8233842/how-to-check-if-directory-exist-using-c-and-winapi 
+{
+	DWORD ftyp = GetFileAttributesW(dirName_in);
+	if (ftyp == INVALID_FILE_ATTRIBUTES)
+		return false;
+
+	if (ftyp & FILE_ATTRIBUTE_DIRECTORY)
+		return true;
+
+	return false;
+}
+
+MIDL_INTERFACE("00000000-0000-0000-0000-000000000000")
+INameSpaceTreeControlValuesPrivate : IUnknown
+{
+public:
+	virtual void stub() = 0;
+	virtual void SetIndentValue(int indent) = 0;
+};
+
+static HRESULT __fastcall CNSCHost_FillNSC(uintptr_t nscHost) //todo: reimplement the filter from 7 shell32, CLSID_PersonalStartMenu GUID_2659b475_eeb8_48b7_8f07_b378810f48cf
+{
+	HRESULT result = S_OK;
+	if (*(DWORD*)(nscHost + 0xCC)) return result;
+
+	WCHAR fallbackPath[MAX_PATH];
+	ExpandEnvironmentStringsW(L"%PROGRAMDATA%\\Microsoft\\Windows\\Start Menu\\Programs", fallbackPath, MAX_PATH);
+
+	WCHAR* sibAllProgs = L"shell:::{865e5e76-ad83-4dca-a109-50dc2113ce9b}";
+
+	IShellItem* ppv;
+	if ((dirExists(sibAllProgs) && SHCreateItemFromParsingName(
+		sibAllProgs,
+		0LL,
+		IID_IShellItem,
+		(void**)&ppv) == S_OK) || SHCreateItemFromParsingName(
+			fallbackPath,
+			0LL,
+			IID_IShellItem,
+			(void**)&ppv) == S_OK)
+	{
+		INameSpaceTreeControl2* control = *(INameSpaceTreeControl2**)(nscHost + 0x70);
+
+		//ideally we would want to queryinterface to get these, but im too lazy to get the guids for these, and i doubt these offsets would change
+		IVisualProperties* visualProps = (IVisualProperties*)(__int64(control) + 0x20);
+		INameSpaceTreeControlValuesPrivate* privatec = (INameSpaceTreeControlValuesPrivate*)(__int64(control) + 0x50);
+		//privatec->SetIndentValue(13);
+		CNscTree_SetIndentValue((uintptr_t)privatec, 13);
+		CNscTree_SetItemHeight((uintptr_t)visualProps, 19);
+		//visualProps->SetItemHeight(19);
+
+		result = control->AppendRoot(ppv, 96, 3, 0);
+		if (SUCCEEDED(result))
+			*(DWORD*)(nscHost + 0xCC) = 1;
+
+		ppv->Release();
+	}
+	return result;
+}
