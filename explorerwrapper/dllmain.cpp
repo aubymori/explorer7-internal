080a704e94f46e7ada18bd58b767b73899638ad2
kfh83 <85626296+kfh83@users.noreply.github.com>
2024-10-04 12:36:59 -0700

    6519 syncthreadproc, should probably work on vista

--- explorerwrapper/dllmain.cpp
+++ explorerwrapper/dllmain.cpp
@@ -976,7 +976,7 @@ void HookTrayThread(void)
 {
 	CTray__SyncThreadProc_orig = (LPTHREAD_START_ROUTINE)FindPattern(
 		(uintptr_t)GetModuleHandle(NULL),
-		"48 89 5C 24 10 48 89 6C 24 18 48 89 74 24 20 57 41 54 41 55 41 56 41 57 48 81 EC 00 03 00 00 48 8B"
+		"48 89 5C 24 10 48 89 6C 24 18 48 89 74 24 20 57 41 54 41 55 41 56 41 57 48 81 EC ?? ?? 00 00 48 8B"
 	);
 
 	if (CTray__SyncThreadProc_orig)
