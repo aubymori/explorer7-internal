d74a8b174d6fa3b132b8aea729518e4e105f1d38
aubymori <aubyomori@gmail.com>
2024-10-08 10:14:24 -0500

    Statically link VC++ runtime This makes sure the user doesn't need to install VC++ redist. DLL size barely grows from this (~126KB -> ~226KB).

--- explorerwrapper/minCRT.cpp
+++ explorerwrapper/minCRT.cpp
@@ -62,28 +62,28 @@ void operator delete[](void* ptr)
 	free(ptr);
 }
 
-void *memcpy(void *dst, const void *src, size_t n)
-{
-	const char *p = (const char*)src;
-	char *q = (char*)dst;
-#if defined(__i386__)
-	size_t nl = n >> 2;
-	asm volatile ("cld ; rep ; movsl ; movl %3,%0 ; rep ; movsb":"+c" (nl),
-		      "+S"(p), "+D"(q)
-		      :"r"(n & 3));
-#elif defined(__x86_64__)
-	size_t nq = n >> 3;
-	asm volatile ("cld ; rep ; movsq ; movl %3,%%ecx ; rep ; movsb":"+c"
-		      (nq), "+S"(p), "+D"(q)
-		      :"r"((uint32_t) (n & 7)));
-#else
-	while (n--) {
-		*q++ = *p++;
-	}
-#endif
-
-	return dst;
-}
+//void *memcpy(void *dst, const void *src, size_t n)
+//{
+//	const char *p = (const char*)src;
+//	char *q = (char*)dst;
+//#if defined(__i386__)
+//	size_t nl = n >> 2;
+//	asm volatile ("cld ; rep ; movsl ; movl %3,%0 ; rep ; movsb":"+c" (nl),
+//		      "+S"(p), "+D"(q)
+//		      :"r"(n & 3));
+//#elif defined(__x86_64__)
+//	size_t nq = n >> 3;
+//	asm volatile ("cld ; rep ; movsq ; movl %3,%%ecx ; rep ; movsb":"+c"
+//		      (nq), "+S"(p), "+D"(q)
+//		      :"r"((uint32_t) (n & 7)));
+//#else
+//	while (n--) {
+//		*q++ = *p++;
+//	}
+//#endif
+//
+//	return dst;
+//}
 
 void *memset(void *dst, int c, size_t n)
 {
