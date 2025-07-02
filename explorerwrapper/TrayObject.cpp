dd6d5221b1620e2de3548b3011f4335da5cdbf1a
Ittr <ittrgrey@gmail.com>
2025-07-02 12:28:22 +0100

    Cleanup TrayObject syntax

--- explorerwrapper/TrayObject.cpp
+++ explorerwrapper/TrayObject.cpp
@@ -2,7 +2,7 @@
 #include "TrayObject.h"
 #include "dbgprint.h"
 
-CSysTrayWrapper::CSysTrayWrapper(IOleCommandTarget *stobject8)
+CSysTrayWrapper::CSysTrayWrapper(IOleCommandTarget* stobject8)
 {
 	m_cRef = 1;
 	m_stobject8 = stobject8;
@@ -14,7 +14,7 @@ CSysTrayWrapper::~CSysTrayWrapper()
 	m_stobject8->Release();
 }
 
-HRESULT STDMETHODCALLTYPE CSysTrayWrapper::QueryInterface(REFIID riid,void **ppvObject)
+HRESULT STDMETHODCALLTYPE CSysTrayWrapper::QueryInterface(REFIID riid, void** ppvObject)
 {
 	dbgprintf(L"CSysTrayWrapper::QueryInterface NOT IMPLEMENTED");
 	return E_NOTIMPL;
@@ -35,12 +35,12 @@ ULONG STDMETHODCALLTYPE CSysTrayWrapper::Release(void)
 	return m_cRef;
 }
 
-HRESULT STDMETHODCALLTYPE CSysTrayWrapper::QueryStatus( const GUID *pguidCmdGroup, ULONG cCmds, OLECMD prgCmds[  ], OLECMDTEXT *pCmdText)
+HRESULT STDMETHODCALLTYPE CSysTrayWrapper::QueryStatus(const GUID* pguidCmdGroup, ULONG cCmds, OLECMD prgCmds[], OLECMDTEXT* pCmdText)
 {	
-	return m_stobject8->QueryStatus(pguidCmdGroup,cCmds,prgCmds,pCmdText);
+	return m_stobject8->QueryStatus(pguidCmdGroup, cCmds, prgCmds, pCmdText);
 }
 
-HRESULT STDMETHODCALLTYPE CSysTrayWrapper::Exec( const GUID *pguidCmdGroup, DWORD nCmdID, DWORD nCmdexecopt, VARIANT *pvaIn, VARIANT *pvaOut)
+HRESULT STDMETHODCALLTYPE CSysTrayWrapper::Exec(const GUID* pguidCmdGroup, DWORD nCmdID, DWORD nCmdexecopt, VARIANT* pvaIn, VARIANT* pvaOut)
 {	
 	//command #2 differs 7->8; also command #4 must be issued later
 	if (pguidCmdGroup && *pguidCmdGroup == CGID_ShellServiceObject && nCmdID == 2)
@@ -53,15 +53,15 @@ HRESULT STDMETHODCALLTYPE CSysTrayWrapper::Exec( const GUID *pguidCmdGroup, DWOR
 	else
 		DoInitSysTray();
 
-	HRESULT ret = m_stobject8->Exec(pguidCmdGroup,nCmdID,nCmdexecopt,pvaIn,pvaOut);
+	HRESULT ret = m_stobject8->Exec(pguidCmdGroup, nCmdID, nCmdexecopt, pvaIn, pvaOut);
 	return ret;
 }
 
-void CSysTrayWrapper::DoInitSysTray( void)
+void CSysTrayWrapper::DoInitSysTray(void)
 {	
 	if (initialized) return;
 	VARIANT v;
-	InitVariantFromUInt32(3,&v);
-	HRESULT ret = m_stobject8->Exec(&CGID_ShellServiceObject,4,0,&v,NULL); //start
+	InitVariantFromUInt32(3, &v);
+	HRESULT ret = m_stobject8->Exec(&CGID_ShellServiceObject, 4, 0, &v, NULL); //start
 	initialized = (ret == S_OK);
 }
