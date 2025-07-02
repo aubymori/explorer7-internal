33bf703e2e11b4e36ff1006fb39fbc6380588eb8
Ittr <ittrgrey@gmail.com>
2025-07-02 12:25:59 +0100

    Cleanup syntax for AuthUI

--- explorerwrapper/AuthUI.h
+++ explorerwrapper/AuthUI.h
@@ -3,9 +3,9 @@
 #include "common.h"
 
 #pragma region GUID definitions
-DEFINE_GUID(CLSID_AuthUIShutdownChoices,0x14CE31DC, 0xABc2, 0x484c, 0xb0,0x61,0xcf,0x34,0x16,0xae,0xd8,0xff); //{14CE31DC-ABC2-484C-B061-CF3416AED8FF}
+DEFINE_GUID(CLSID_AuthUIShutdownChoices, 0x14CE31DC, 0xABc2, 0x484c, 0xb0, 0x61, 0xcf, 0x34, 0x16, 0xae, 0xd8, 0xff); //{14CE31DC-ABC2-484C-B061-CF3416AED8FF}
 DEFINE_GUID(IID_IShutdownChoices7, 0x0F678FCDE, 0x0EB44, 0x4B6E, 0x9B, 0x75, 0x0CC, 0x4A, 0x66, 0x1F, 0x52, 0x63); //f678fcde_eb44_4b6e_9b75_cc4a661f5263
-DEFINE_GUID(IID_IShutdownChoices8,0x811CA537, 0x0FEEC, 0x4041, 0x9F, 0x51, 0x0E7, 0x0BD, 0x0A5, 0x96, 0x60, 0x1C); //811ca537_feec_4041_9f51_e7bda596601c
+DEFINE_GUID(IID_IShutdownChoices8, 0x811CA537, 0x0FEEC, 0x4041, 0x9F, 0x51, 0x0E7, 0x0BD, 0x0A5, 0x96, 0x60, 0x1C); //811ca537_feec_4041_9f51_e7bda596601c
 DEFINE_GUID(IID_IShutdownChoices10, 0xbfdc5e2f, 0x3402, 0x49b3, 0x87, 0x40, 0x91, 0xd6, 0xdc, 0x5d, 0xbb, 0x15);
 
 DEFINE_GUID(IID_IAuthUILogonSound7, 0x9E1AA054, 0x1F71, 0x4C1E, 0x0AC, 0x0CC, 0x49, 0x4C, 0x5, 0x1F, 0x0B3, 0x39);
@@ -24,8 +24,8 @@ public:
 	STDMETHOD(GetChoiceEnumerator)(IUnknown**) PURE;
 	STDMETHOD(GetDefaultChoice)(ULONG*) PURE;
 	STDMETHOD(UserHasShutdownRights)(void) PURE;
-	STDMETHOD(GetChoiceName)(ULONG,int,LPWSTR,UINT) PURE;
-	STDMETHOD(GetChoiceDesc)(ULONG,LPWSTR,UINT) PURE;
+	STDMETHOD(GetChoiceName)(ULONG, int, LPWSTR, UINT) PURE;
+	STDMETHOD(GetChoiceDesc)(ULONG, LPWSTR, UINT) PURE;
 };
 
 MIDL_INTERFACE("811ca537-feec-4041-9f51-e7bda596601c")
@@ -41,8 +41,8 @@ public:
 	STDMETHOD(GetChoiceEnumerator)(IUnknown**) PURE;
 	STDMETHOD(GetDefaultChoice)(ULONG*) PURE;
 	STDMETHOD(UserHasShutdownRights)(void) PURE;
-	STDMETHOD(GetChoiceName)(ULONG,int,LPWSTR,UINT) PURE;
-	STDMETHOD(GetChoiceDesc)(ULONG,LPWSTR,UINT) PURE;
+	STDMETHOD(GetChoiceName)(ULONG, int, LPWSTR, UINT) PURE;
+	STDMETHOD(GetChoiceDesc)(ULONG, LPWSTR, UINT) PURE;
 };
 
 MIDL_INTERFACE("bfdc5e2f-3402-49b3-8740-91d6dc5dbb15")
