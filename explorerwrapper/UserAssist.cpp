a31eeae913f9ad654931f9e2aceba1517377585f
Ittr <ittrgrey@gmail.com>
2025-07-24 10:25:29 +0100

    Cleanup UserAssist syntax

--- explorerwrapper/UserAssist.cpp
+++ explorerwrapper/UserAssist.cpp
@@ -14,27 +14,34 @@ extern "C" HRESULT WINAPI Explorer_CoCreateInstance(
 HRESULT WINAPI UAQueryEntry(REFIID iid, LPWSTR parsingname, PUEMINFO uem)
 {	
 	if (!g_UserAssist) //bugbug shpindllofclsid?
-		Explorer_CoCreateInstance(CLSID_UserAssist,NULL,CLSCTX_INPROC_SERVER || CLSCTX_INPROC_HANDLER || CLSCTX_NO_CODE_DOWNLOAD,IID_IUserAssist7,(PVOID*)&g_UserAssist);
-	if ( g_UserAssist )
-		return g_UserAssist->QueryEntry(iid,parsingname,uem);
+	{
+		Explorer_CoCreateInstance(CLSID_UserAssist, NULL, CLSCTX_INPROC_SERVER || CLSCTX_INPROC_HANDLER || CLSCTX_NO_CODE_DOWNLOAD, IID_IUserAssist7, (PVOID*)&g_UserAssist);
+	}
+
+	if (g_UserAssist)
+	{
+		return g_UserAssist->QueryEntry(iid, parsingname, uem);
+	}
 	else
+	{
 		return E_NOINTERFACE;
+	}
 }
 
 HRESULT WINAPI UAQueryShortcut(LPITEMIDLIST pidl, PUEMINFO uem)
 {
 	HRESULT result;
 	IShellItem* shellitem;
-	result = SHCreateItemFromIDList(pidl,IID_IShellItem,(LPVOID*)&shellitem);
-	if ( SUCCEEDED(result) )
+	result = SHCreateItemFromIDList(pidl, IID_IShellItem, (LPVOID*)&shellitem);
+	if (SUCCEEDED(result))
 	{
 		LPWSTR parsingname;
-		result = shellitem->GetDisplayName(SIGDN_DESKTOPABSOLUTEPARSING,&parsingname);
-		if ( SUCCEEDED(result) )
+		result = shellitem->GetDisplayName(SIGDN_DESKTOPABSOLUTEPARSING, &parsingname);
+		if (SUCCEEDED(result))
 		{
 			uem->cbSize = sizeof(UEMINFO);
 			uem->dwMask = 0x31;			
-			result = UAQueryEntry(UAIID_SHORTCUTS,parsingname,uem);		
+			result = UAQueryEntry(UAIID_SHORTCUTS, parsingname, uem);		
 			CoTaskMemFree(parsingname);
 		}
 	}
@@ -43,7 +50,7 @@ HRESULT WINAPI UAQueryShortcut(LPITEMIDLIST pidl, PUEMINFO uem)
 
 LPWSTR WINAPI CoAllocString(LPWSTR src)
 {	
-	LPWSTR dst = (LPWSTR)CoTaskMemAlloc(((lstrlenW(src)+1)*sizeof(WCHAR)));
-	lstrcpyW(dst,src);
+	LPWSTR dst = (LPWSTR)CoTaskMemAlloc(((lstrlenW(src) + 1) * sizeof(WCHAR)));
+	lstrcpyW(dst, src);
 	return dst;
 }
